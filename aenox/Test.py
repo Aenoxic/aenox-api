8214da28bdbe96c8a43fada19efdd1710d7e7747
Aenoxic <max.melzer@yahoo.com>
2024-08-31 16:52:40 +0200

    version changed -> `0.0.6`

--- aenox/Test.py
+++
@@ -1,8 +0,0 @@
-from aenox import AenoXAPI
-from datetime import datetime
-
-if __name__ == "__main__":
-    api = AenoXAPI(api_key="1b18d866-59f4-4366-8a5a-b55ebc78845c")
-    claimed = api.get_guild_stats(1262732535990059009).language
-    print(claimed)
-    print(f"Type: {type(claimed)}")
--- aenox/__init__.py
+++ aenox/__init__.py
@@ -1,5 +1,5 @@
 """A test PyPI package."""
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 from .api import AenoXAPI
 from .errors import *
